632e51a9012c9aacd07b2f4cf92db3f9348a3446
MuntadherAteeq <mun.ateeq@outlook.com>
2025-04-21 15:26:48 +0300

    refactor: simplify AccountList component by removing conditional rendering for empty user records

--- src/components/Account/Account-List.tsx
+++ src/components/Account/Account-List.tsx
@@ -11,27 +11,9 @@ import AccountTable from "./Account-Table";
 export async function AccountList(props: { Activity: string }) {
   const users = await getAccounts();
   return (
-    <>
-      {(users ?? []).length > 0 ? (
-        <AccountTable users={users} />
-      ) : (
-        <div className="py-12 flex flex-col items-center justify-center text-center space-y-4">
-          <div className="bg-muted p-4 rounded-full">
-            <Inbox className="h-12 w-12 text-primary" />
-          </div>
-          <div className="space-y-2">
-            <h3 className="text-xl font-semibold">No Records</h3>
-            <p className="text-muted-foreground max-w-sm mx-auto">
-              You haven't added any Records to your collection. Add your first
-              Record to get started.
-            </p>
-          </div>
-          <AddNewRecord />
-        </div>
-      )}
-      {/* </CardContent> */}
-      {/* </Card> */}
-    </>
+    <div className="w-full h-full p-2">
+      <AccountTable users={users} />
+    </div>
   );
 }
 
