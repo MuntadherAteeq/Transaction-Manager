78c29b663adada14b7af51d813cfe6cbe3643c3e
MuntadherAteeq <mun.ateeq@gmail.com>
2025-05-01 10:45:52 +0300

    feat: add UI components for adding and deleting parts in PartsTable

--- src/app/App/JobCards/Parts-Table.tsx
+++ src/app/App/JobCards/Parts-Table.tsx
@@ -8,6 +8,9 @@ import { Part } from "@prisma/client";
 import { AllCommunityModule, ColDef, ModuleRegistry } from "ag-grid-community";
 import { ClientSideRowModelModule } from "ag-grid-community"; // Import the missing module
 import { useTableTheme } from "@/hooks/use-TableTheme";
+import { Card } from "@/components/ui/card";
+import { PlusCircleIcon, Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 // Register the required modules
 ModuleRegistry.registerModules([AllCommunityModule, ClientSideRowModelModule]);
@@ -15,68 +18,7 @@ ModuleRegistry.registerModules([AllCommunityModule, ClientSideRowModelModule]);
 export default function PartTable() {
   const tableTheme = useTableTheme();
 
-  const [rowData, setRowData] = useState<Part[]>([
-    {
-      id: 1,
-      description: "Part 1",
-      qty: 10,
-      rate: 100,
-      amount: 1000,
-      partCode: "",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      serviceId: 0,
-      jobCardId: null,
-    },
-    {
-      id: 2,
-      description: "Part 2",
-      qty: 5,
-      rate: 200,
-      amount: 1000,
-      partCode: "",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      serviceId: 0,
-      jobCardId: null,
-    },
-    {
-      id: 3,
-      description: "Part 3",
-      qty: 8,
-      rate: 150,
-      amount: 1200,
-      partCode: "",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      serviceId: 0,
-      jobCardId: null,
-    },
-    {
-      id: 4,
-      description: "Part 4",
-      qty: 12,
-      rate: 80,
-      amount: 960,
-      partCode: "",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      serviceId: 0,
-      jobCardId: null,
-    },
-    {
-      id: 5,
-      description: "Part 5",
-      qty: 20,
-      rate: 50,
-      amount: 1000,
-      partCode: "",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      serviceId: 0,
-      jobCardId: null,
-    },
-  ]);
+  const [rowData, setRowData] = useState<Part[]>([]);
 
   // Column Definitions: Defines the columns to be displayed.
   const [colDefs, setColDefs] = useState<ColDef[]>([
@@ -113,8 +55,7 @@ export default function PartTable() {
         }
         return false;
       },
-      onCellValueChanged: (params: { data: { amount: number } }) => {
-      },
+      onCellValueChanged: (params: { data: { amount: number } }) => {},
     },
     // Rate column
     {
@@ -133,8 +74,7 @@ export default function PartTable() {
         }
         return false;
       },
-      onCellValueChanged: (params: { data: { amount: number } }) => {
-      },
+      onCellValueChanged: (params: { data: { amount: number } }) => {},
     },
     // amount column
     {
@@ -159,6 +99,24 @@ export default function PartTable() {
 
   return (
     <div className="w-full h-full">
+      <Card className="rounded-none p-3 flex flex-row gap-3">
+        <Button
+          type="button"
+          variant={"ghost"}
+          className="border-1 hover:border-1 hover:text-primary"
+        >
+          <PlusCircleIcon className="w-4 h-4 " />
+          New
+        </Button>
+        <Button
+          variant={"ghost"}
+          className="border-1 hover:border-1 hover:text-destructive-foreground"
+          type="button"
+        >
+          <Trash />
+          Delete
+        </Button>
+      </Card>
       <AgGridReact
         theme={tableTheme}
         className="h-full w-full"
