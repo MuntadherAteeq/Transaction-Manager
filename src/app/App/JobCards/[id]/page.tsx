05c64e892e09b8134eea208799c3ea3ab1011a01
MuntadherAteeq <mun.ateeq@outlook.com>
2025-05-13 00:54:44 +0300

    feat: import prisma client in JobCardPreview component for database access

--- src/app/App/JobCards/[id]/page.tsx
+++ src/app/App/JobCards/[id]/page.tsx
@@ -2,6 +2,7 @@
 import { redirect } from "next/navigation";
 import { JobCardForm } from "../Job-Card-Form";
 import { JobCardFormProvider } from "../form-store";
+import prisma from "@/lib/prisma";
 
 export default async function JobCardPreview(props: any) {
   const id = (await props.params).id;
